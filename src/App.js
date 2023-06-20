a3bc49f6c3aef13e95a103ccd06a1517c4859ec9
LuMoraesT <lucianamoraestx@gmail.com>
2023-06-20 16:12:04 -0300

    finalizando o projeto

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import Video from "./pages/Video";
 import db from "./config/firebase";
+
 import { collection, getDocs } from "firebase/firestore/lite";
 
 function App() {
